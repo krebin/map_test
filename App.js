bf0c4f538cc8529aab015b735b5d1ce9fa25a1ab
krebin <vvn012@ucsd.edu>
2017-11-14 19:47:45 -0800

    no crashes

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import Mapbox from '@mapbox/react-native-mapbox-gl';
+//import { isUndefined, isFunction } from './javascript/utils'
+
+import { NativeModules, NativeEventEmitter } from 'react-native';
+const MapboxGL = NativeModules.MGLModule;
 
 Mapbox.setAccessToken('pk.eyJ1Ijoia3JlYmluIiwiYSI6ImNqOXRyN2NpNjAxbDUyeG9lcnVxNXV3aHYifQ.Co5xDA25ehe16YgaFk0t2w');
 
@@ -27,9 +31,28 @@ class MapScreen extends React.Component {
     static navigationOptions = {
         title: 'Map',
     };
+
+
     render() {
         return (
             <View style={styles.container}>
+                <Button
+                    onPress={async () =>
+                    {
+                        alert('button pressed')
+                        const progressListener = (offlineRegion, status) => console.log(offlineRegion, status);
+                        const errorListener = (offlineRegion, err) => console.log(offlineRegion, err);
+
+                        await MapboxGL.offlineManager.createPack({
+                            name: 'offlinePack',
+                            styleURL: 'mapbox://...',
+                            minZoom: 14,
+                            maxZoom: 20,
+                            bounds: [[100, 100], [100, 100]]
+                        }, progressListener, errorListener)}}
+                    title={"Download Map"}
+                    style={{borderWidth: 1, borderColor: 'blue'}}
+                />
                 <Mapbox.MapView
                     styleURL={Mapbox.StyleURL.Basic}
                     zoomLevel={15}
